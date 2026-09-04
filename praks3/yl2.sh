37d588c3f67d05cb498a91f37a0f630c066c2d90
Joonas Mägi <joonas.magi@voco.ee>
2026-09-04 06:20:44 -0400

    Küpsisetordi valmistamise script

---
+++ praks3/yl2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Skripti eesmärk: Arvutab tordi ja küpsiste mõõtmete põhjal vajaliku pakkide arvu.
+
+echo -n "Sisesta aluskandiku pikkus (cm): "
+read kandik_pikkus
+
+echo -n "Sisesta aluskandiku laius (cm): "
+read kandik_laius
+
+echo -n "Sisesta ühe küpsise pikkus (cm): "
+read kypsis_pikkus
+
+echo -n "Sisesta ühe küpsise laius (cm): "
+read kypsis_laius
+
+echo -n "Mitu kihti tordil on: "
+read kihte
+
+echo -n "Mitu küpsist on ühes pakis: "
+read kypsiseid_pakis
+
+# Arvutused expr abil
+pikkusesse=$(expr $kandik_pikkus / $kypsis_pikkus)
+laiusesse=$(expr $kandik_laius / $kypsis_laius)
+kypsiseid_kihis=$(expr $pikkusesse \* $laiusesse)
+kypsised_kokku=$(expr $kypsiseid_kihis \* $kihte)
+
+# Pakkide arvu ülesümardamine expr-iga: (kokku + pakis - 1) / pakis
+pakkide_arv=$(expr \( $kypsised_kokku + $kypsiseid_pakis - 1 \) / $kypsiseid_pakis)
+
+echo "Tordi jaoks on vaja kokku $kypsised_kokku küpsist."
+echo "Poest tuleb osta $pakkide_arv pakki küpsiseid."
